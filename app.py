b2f3aa3ae45872dd7a3ec909f9260e0e26249f29
oussamasf <59108871+oussamasf@users.noreply.github.com>
2021-11-23 16:48:34 +0100

    first commit ever

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask ,render_template ,request
+import numpy as np
+import joblib 
+import sys
+import os
+
+
+app = Flask(__name__) 
+inputs = np.zeros(shape = [1,9])
+
+
+LOCATION = 'OneDrive\Bureau\plentina\model'
+model_path = os.path.join(LOCATION, 'model.pkl')
+scaler_path = os.path.join(LOCATION, 'scaler.pkl')
+
+model  = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/prediction' , methods = ['POST'])
+def greeting():
+
+    inputs[0,0] = float(request.form.get("step"))
+    inputs[0,1] = float(request.form.get("type"))
+    inputs[0,2] = float(request.form.get("amount"))
+    inputs[0,3] = float(request.form.get("oldbalanceOrig"))
+    inputs[0,4] = float(request.form.get("newbalanceOrig"))
+    inputs[0,5] = float(request.form.get("oldbalanceDest"))
+    inputs[0,6] = float(request.form.get("newbalanceDest"))
+
+    if inputs[0,5] == 0 and inputs[0,6] == 0: 
+        inputs[0,5] , inputs[0,6] = -1 , -1 
+
+    inputs[0,7] = inputs[0,4] + inputs[0,2] - inputs[0,3]
+    inputs[0,8] = inputs[0,5] + inputs[0,2] - inputs[0,6]
+    
+    X_test = scaler.transform(inputs)
+
+    predicted = model.predict(X_test)
+
+    if predicted == [1] :
+        a = "scam"
+    else : a = 'fine'
+
+    return render_template('prediction.html', a = a , b = X_test )
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        port = 80
+
+    app.run(host='0.0.0.0', port=port, debug=True)
